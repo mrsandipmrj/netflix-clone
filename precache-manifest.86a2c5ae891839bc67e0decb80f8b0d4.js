cc6e39e04b77dfa3150c5efaff51529ad51d18c6
mrsandipmrj <mrsandipmrj@gmail.com>
2023-05-11 11:31:45 +0530

    Updates

---
+++ precache-manifest.86a2c5ae891839bc67e0decb80f8b0d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a8cea374c3b441b6ce",
+    "url": "/netflix-clone/static/css/main.bad03a77.chunk.css"
+  },
+  {
+    "revision": "51a8cea374c3b441b6ce",
+    "url": "/netflix-clone/static/js/main.51a8cea3.chunk.js"
+  },
+  {
+    "revision": "4718a22e8f46a63c8878",
+    "url": "/netflix-clone/static/js/1.4718a22e.chunk.js"
+  },
+  {
+    "revision": "0b892cc98d3bb0874451",
+    "url": "/netflix-clone/static/js/runtime~main.0b892cc9.js"
+  },
+  {
+    "revision": "fa81ed5818b831a937ed25c285291b91",
+    "url": "/netflix-clone/static/media/bg.fa81ed58.jpg"
+  },
+  {
+    "revision": "a1eb9b1b97fe41b828ede9c02232596e",
+    "url": "/netflix-clone/index.html"
+  }
+];
